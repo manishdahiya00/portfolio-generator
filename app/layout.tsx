bebcd0920a98fe43177ecc3b18296857c6096d02
Manish Dahiya <manishdahiya0944@gmail.com>
2025-01-01 14:57:39 +0530

    change default theme to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider defaultTheme="system">{children}</ThemeProvider>
+        <ThemeProvider defaultTheme="light">{children}</ThemeProvider>
       </body>
     </html>
   );
